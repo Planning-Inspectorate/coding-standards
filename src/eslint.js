a89407648a1058dca04c74c8e77306791b9c330b
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-21 14:40:49 +0100

    feat(eslint): better support for ts/js mixed projects

--- src/eslint.js
+++ src/eslint.js
@@ -2,32 +2,41 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import checkFile from 'eslint-plugin-check-file';
 import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 /**
  * @type {import('@eslint/config-helpers').ConfigWithExtendsArray}
  */
 export default [
-	eslint.configs.recommended,
-	tseslint.configs.recommended,
-	globalIgnores([
-		'.husky',
-		'dist/**',
-		'node_modules/**',
-		'**/*.test.ts',
-		'**/.static/**',
-		'packages/database/src/client/**'
-	]),
-	eslintConfigPrettier,
 	{
+		files: ['**/*.{js,ts}'],
+		extends: [eslint.configs.recommended],
+		languageOptions: {
+			globals: globals.node
+		}
+	},
+	{
+		files: ['**/*.ts'],
+		extends: [tseslint.configs.recommended],
 		rules: {
 			'@typescript-eslint/consistent-type-imports': 'error',
 			'@typescript-eslint/no-explicit-any': 'off'
 		}
 	},
+	globalIgnores([
+		'.husky',
+		'dist/**',
+		'node_modules/**',
+		'**/*.test.ts',
+		'**/*.test.js',
+		'**/.static/**',
+		'packages/database/src/client/**',
+		'*.config.js'
+	]),
+	eslintConfigPrettier,
 	{
 		files: ['**/*.{js,ts}'],
-		ignores: ['tmp/**/*.*', 'node_modules/**/*.*'],
 		plugins: {
 			'check-file': checkFile
 		},
