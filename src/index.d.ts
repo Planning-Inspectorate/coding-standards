63ba98acc345ba65d349418f53fb1c622e383376
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-24 14:53:29 +0100

    fix: add type definitions for typescript support

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+import type { UserConfig as CommitlintConfig } from '@commitlint/types';
+import type { ConfigWithExtendsArray as EslintConfig } from '@eslint/config-helpers';
+import type { Config as PrettierConfig } from 'prettier';
+import type { Config as StylelintConfig } from 'stylelint';
+
+export declare const commitlintConfig: CommitlintConfig;
+export declare const eslintConfig: EslintConfig;
+export declare const prettierConfig: PrettierConfig;
+export declare const stylelintConfig: StylelintConfig;
