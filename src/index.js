da488eb28e0b5bd70540bf25c4f423f600ff2922
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-24 13:39:12 +0100

    feat: stylelint config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import commitlintConfig from './commitlint.js';
 import eslintConfig from './eslint.js';
 import prettierConfig from './prettier.js';
+import stylelintConfig from './stylelint.js';
 
-export { commitlintConfig, eslintConfig, prettierConfig };
+export { commitlintConfig, eslintConfig, prettierConfig, stylelintConfig };
